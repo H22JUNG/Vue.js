44d8b09b252d5888c457c7575ebff5a0cd7f59a5
Heejung Kim <h2jung6875@naver.com>
2023-05-15 21:36:16 +0900

    Todo 앱 만들기

---
+++ src/components/Todo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      <input type="checkbox" :checked="todo.checked">
+      <span class="ml-3">{{ todo.text }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    }
+  }
+};
+</script>

056e73ede3edbee29e7394767da51ee15435b633
Heejung Kim <h2jung6875@naver.com>
2023-05-15 21:37:08 +0900

    Todo 앱 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,47 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-</template>
+  <div id="app" class="container">
+    <h1>Todo App</h1>
+    <input
+    v-model="todoText"
+      type="text"
+      class="w-100 p-2"
+      placeholder="Type todo"
+      @keyup.enter="addTodo"
+    />
+    <hr />
 
+    <Todo v-for="todo in todos" :key="todo.id" :todo="todo" />
+    <!-- <Todo /> -->
+  </div>
+</template>
+<script>
+import Todo from "@/components/Todo.vue";
+export default {
+  components: {
+    Todo
+  },
+  data() {
+    return {
+      todoText : '',
+      todos: [
+        { id: 1, text: "buy a car", checked: true },
+        { id: 2, text: "play game", checked: false },
+      ]
+    }
+  },
+
+  methods: {
+    addTodo(e) {
+      this.todos.push({
+        id : Math.random(),
+        text : e.target.value,
+        checked : false
+      })
+      this.todoText = '';
+    },
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
